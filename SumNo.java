cb45fc8500746821ae95418d780006a204193ef8
ELIZABETHNJ <elizabethnj1996@gmail.com>
2017-07-04 15:21:44 +0530

    Create SumNo.java

---
+++ SumNo.java
@@ -0,0 +1,22 @@
+# n-num
+import java.io.*;
+import java.util.*;
+public class SumNo
+{
+public static void main(String args[])
+{
+int x,sum=0;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the integer");
+x=sc.nextInt();
+for(int i=1;i<=x;++i)
+{
+sum=sum+i;
+}
+System.out.println(sum);
+}
+}
+
+    Contact GitHub API Training Shop Blog About 
+
+    © 2017 GitHub, Inc. Terms Privacy Security Status He
